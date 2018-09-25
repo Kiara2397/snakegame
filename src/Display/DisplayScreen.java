9486c459faff149989c09d9d4c30b08527094918
aleRigau <alerigau@ppc-pr.com>
2018-09-24 21:15:20 -0400

    Changed Background color and Grid color

--- src/Display/DisplayScreen.java
+++ src/Display/DisplayScreen.java
@@ -47,7 +47,7 @@ public class DisplayScreen {
         canvas.setMaximumSize(new Dimension(width, height));
         canvas.setMinimumSize(new Dimension(width, height));
         canvas.setFocusable(false);
-        canvas.setBackground(Color.black);
+        canvas.setBackground(Color.decode("#192a56"));
 
         frame.add(canvas);
         frame.pack();
